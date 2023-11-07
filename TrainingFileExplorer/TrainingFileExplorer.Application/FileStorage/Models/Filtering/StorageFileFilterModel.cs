8fb57bb4961284f906374f30e8c1fc972e534237
Mirzohid-Mominov <momirzohid4@gmail.com>
2023-11-07 19:19:16 +0500

    updated

--- TrainingFileExplorer/TrainingFileExplorer.Application/FileStorage/Models/Filtering/StorageFileFilterModel.cs
+++ TrainingFileExplorer/TrainingFileExplorer.Application/FileStorage/Models/Filtering/StorageFileFilterModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using TrainingFileExplorer.Aplication.Common.Models.Filtering;
 
@@ -9,6 +10,7 @@ namespace TrainingFileExplorer.Application.FileStorage.Models.Filtering
 {
     public class StorageFileFilterModel : FilterPagination
     {
+        [JsonIgnore]
         public string DirectoryPath { get; set; } = string.Empty;
 
         public ICollection<StorageFileType> FileTypes { get; set; } = default!;
